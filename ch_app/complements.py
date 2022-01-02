8e6bdca7a6fa9261748f1ef77487d8fa4e0b4966
Douglas Vitor <douglas_oliveiravitor@hotmail.com>
2022-01-02 20:46:40 -0300

    .

--- ch_app/complements.py
+++ ch_app/complements.py
@@ -265,8 +265,8 @@ def statusRGB():
     for rgb in models.R_wifi.objects.filter(type2='RGB'):
         try:
             checkRgb = request.get("http://" + rgb.ip + "/json/state", timeout=2)
-            #decodeRgb = json.loads(checkRgb.content.decode('utf-8'))
-            rgbs_status[rgb.ip] = checkRgb
+            decodeRgb = json.loads(checkRgb.content.decode('utf-8'))
+            rgbs_status[rgb.ip] = decodeRgb
             
         except:
             pass
